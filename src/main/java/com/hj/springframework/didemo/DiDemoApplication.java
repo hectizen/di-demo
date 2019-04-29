1059bf4347540501490e29e30c5e39979847b109
hectizen <leejw999@gmail.com>
2019-04-29 22:01:55 +0930

    re-commit

---
+++ src/main/java/com/hj/springframework/didemo/DiDemoApplication.java
@@ -0,0 +1,18 @@
+package com.hj.springframework.didemo;
+
+import com.hj.springframework.didemo.controller.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+        MyController myController = (MyController)ctx.getBean("myController");
+        System.out.println(myController.hello());
+    }
+
+}
